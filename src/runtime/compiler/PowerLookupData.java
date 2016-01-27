020af695e02f72518b2d6ce9f380b3e73656c975
Jeff McAffee <jeff@ktechsystems.com>
2016-01-27 16:39:20 -0700

    Add support for 'True Action:' keywords in PLKs
    Closes #1

--- src/runtime/compiler/PowerLookupData.java
+++ src/runtime/compiler/PowerLookupData.java
@@ -20,8 +20,19 @@ public class PowerLookupData {
 
 		public String getName(){return this.name;}
 		public String getType(){return this.typ;}
-		public String getOp(){return this.op;}
+		public String getOp(){ return stripPrefix(this.op);}
 		public String getCast() {return cast;}
+
+		String stripPrefix(String val) {
+			if (val.startsWith("True Action:")) {
+				// Don't strip the True Action: prefix if the op is 'True Action:Condition'
+				if (!val.endsWith("Condition")) {
+					return val.substring(12);
+				}
+			}
+
+			return val;
+		}
 	}
 
 	public String name 		= new String();
@@ -30,6 +41,4 @@ public class PowerLookupData {
 	public ArrayList<VarOp> operations = new ArrayList<VarOp>();
 	
 	public ArrayList<ArrayList<String>> values = new ArrayList<ArrayList<String>>();
-	
-
 }
--- test/runtime/reader/CsvPowerLookupFileTest.java
+++ test/runtime/reader/CsvPowerLookupFileTest.java
@@ -4,6 +4,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+
+import runtime.compiler.PowerLookupData;
 import runtime.helpers.TestHelper;
 
 import static org.junit.Assert.*;
@@ -12,24 +16,55 @@ import static org.junit.Assert.*;
  * Created by jeff on 1/27/16.
  */
 public class CsvPowerLookupFileTest extends TestHelper {
+    String		TESTDIR				= "gdl/tests/csv_powerlookup_file";
+    String		TEST_PLK_FILE 	    = TESTDIR+"/test_plk.csv";
+    String		TEST_TRUE_ACTION_PLK_FILE = TESTDIR+"/plk_with_true_action_keywords.csv";
+
+    CsvPowerLookupFile subject = null;
 
     @Before
     public void setUp() throws Exception {
-
+        subject = new CsvPowerLookupFile();
     }
 
     @After
     public void tearDown() throws Exception {
-
+        subject = null;
     }
 
     @Test
     public void testParse() throws Exception {
+        subject.parse(TEST_PLK_FILE);
+    }
 
+    @Test
+    public void testParseThrowsFileNotFoundException() throws Exception {
+        try {
+            subject.parse("NON_EXISTING_FILE_PATH");
+        } catch (FileNotFoundException e) {
+            return;
+        }
+
+        fail( "Expecting FileNotFoundException but no exception thrown");
     }
 
     @Test
     public void testExtractLookupsTo() throws Exception {
+        subject.parse(TEST_PLK_FILE);
 
+        HashMap<String,PowerLookupData> lkupData = new HashMap<>();
+        subject.extractLookupsTo(lkupData);
+
+        assert(!lkupData.isEmpty());
+    }
+
+    @Test
+    public void testExtractLookupsToWithTrueActionAssigns() throws Exception {
+        subject.parse(TEST_TRUE_ACTION_PLK_FILE);
+
+        HashMap<String,PowerLookupData> lkupData = new HashMap<>();
+        subject.extractLookupsTo(lkupData);
+
+        assert(!lkupData.isEmpty());
     }
 }
