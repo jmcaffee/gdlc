cbbd3e73d10574c2993e516400cd2360bba5ed01
Jeff McAffee <jeff@ktechsystems.com>
2016-01-27 16:36:00 -0700

    Add CSV file tests

---
+++ test/runtime/reader/CsvLookupFileTest.java
@@ -0,0 +1,58 @@
+package runtime.reader;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jeff on 1/26/16.
+ */
+public class CsvLookupFileTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testParse() throws Exception {
+
+    }
+
+    @Test
+    public void testExtractLookupsTo() throws Exception {
+
+    }
+
+    @Test
+    public void testIsLookupTag() throws Exception {
+
+    }
+
+    @Test
+    public void testIsLookupName() throws Exception {
+
+    }
+
+    @Test
+    public void testAddColumnMins() throws Exception {
+
+    }
+
+    @Test
+    public void testAddColumnMaxs() throws Exception {
+
+    }
+
+    @Test
+    public void testAddRowValues() throws Exception {
+
+    }
+}
---
+++ test/runtime/reader/CsvPowerLookupFileTest.java
@@ -0,0 +1,35 @@
+package runtime.reader;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import runtime.helpers.TestHelper;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jeff on 1/27/16.
+ */
+public class CsvPowerLookupFileTest extends TestHelper {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testParse() throws Exception {
+
+    }
+
+    @Test
+    public void testExtractLookupsTo() throws Exception {
+
+    }
+}
